86ac5f5228a7af1d4e95d27886d3ad543103a0a6
Tests-man <pex_zii@hotmail.com>
2017-07-20 16:50:26 +0700

    20-07-60 16:49
    quotation list
    company list
    contact list
    invoice list
    managemant list

--- quotation/assets/js/datatable.js
+++ quotation/assets/js/datatable.js
@@ -3,10 +3,63 @@ $(document).ready(function() {
   
   function table_report() {
   var table = $('#report').DataTable( {
-	  responsive: true
+	  responsive: true,
+	  bFilter: false,
+	  bInfo: false,
+	  "bPaginate": false
+  });
+  }
+  
+  function table_quotation_list() {
+  var table = $('#quotation_list').DataTable( {
+	  responsive: true,
+	  bFilter: false,
+	  bInfo: false,
+	  "bPaginate": false
+  });
+  }
+  
+  function table_invoice_list() {
+  var table = $('#invoice_list').DataTable( {
+	  responsive: true,
+	  bFilter: false,
+	  bInfo: false,
+	  "bPaginate": false
+  });
+  }
+  function table_company_list() {
+  var table = $('#company_list').DataTable( {
+	  responsive: true,
+	  bFilter: false,
+	  bInfo: false,
+	  "bPaginate": false
+  });
+  }
+  
+  function table_contact_list() {
+  var table = $('#contact_list').DataTable( {
+	  responsive: true,
+	  bFilter: false,
+	  bInfo: false,
+	  "bPaginate": false
+  });
+  }
+  
+  function table_manage_list() {
+  var table = $('#manage_list').DataTable( {
+	  responsive: true,
+	  bFilter: false,
+	  bInfo: false,
+	  "bPaginate": false
   });
   }
   
   table_report();
+  table_quotation_list();
+  table_invoice_list();
+  table_company_list();
+  table_contact_list();
+  table_manage_list();
 });
+
 
